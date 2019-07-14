c1d8517f1c6765eaa74ce108e01227617047cb20
ALVARLEUNG <alvar.leung@qq.com>
2019-07-14 23:17:57 +0800

    refactor: refactor the function of printReceipt and update test

--- main/main.js
+++ main/main.js
@@ -13,30 +13,35 @@ const isCartItemValid = (cartItem) => {
 
 const getItemList = (cartItems) => {
     let goodsBarcodeList = loadAllItems();
+    let cartList = [];
     let itemList = [];
-    // goodsBarcodeList.forEach(item => {
-    //     if (cartItems.indexOf(item.barcode) >= 0) {
-    //         if (itemList.length != 0) {
-    //             for (let i = 0; i < itemList.length; i++) {
-    //                 if (itemList[i].barcode === item) {
-    //                     itemList[i].count++;
-    //                     break;
-    //                 } else {
-    //                     itemList.push({
-    //                         barcode: item.barcode,
-    //                         name: item.name,
-    //                         unit: item.unit,
-    //                         price: item.price,
-    //                         count: 0,
-    //                         promotion: '',
-    //                         cost: 0.0
-    //                     });
-    //                     break;
-    //                 }
-    //             }
-    //         }
-    //     }
-    // });
+    cartItems.forEach(barcode => {
+        let index = barcode.indexOf('-');
+        let subBarcode = '';
+        if (index > -1) {
+            subBarcode = barcode.substring(0, index);
+            if (cartList[subBarcode]) {
+                cartList[subBarcode] += parseFloat(barcode.substring(index + 1));
+            } else cartList[subBarcode] = parseFloat(barcode.substring(index + 1));
+        } else if (index === -1) {
+            if (cartList[barcode]) {
+                cartList[barcode]++;
+            } else cartList[barcode] = 1;
+        }
+    })
+    goodsBarcodeList.forEach(item => {
+        if (cartList[item.barcode]) {
+            itemList.push({
+                barcode: item.barcode,
+                name: item.name,
+                unit: item.unit,
+                price: item.price.toFixed(2),
+                count: cartList[item.barcode],
+                promotion: '',
+                cost: 0
+            });
+        }
+    })
     return itemList;
 };
 
@@ -53,11 +58,11 @@ const getItemPromotion = (itemList) => {
 };
 
 const calculateItemCost = (itemList) => {
-    itemList.forEach(item => {
-        if (item.type === 'BUY_TWO_GET_ONE_FREE') {
-            item.cost = item.count * item.price - parseInt(item.count / 2) * item.price;
+    getItemPromotion(itemList).forEach(item => {
+        if (item.promotion === 'BUY_TWO_GET_ONE_FREE') {
+            item.cost = (item.count * item.price - parseInt(item.count / 3) * parseFloat(item.price)).toFixed(2);
         } else {
-            item.cost = item.count * item.price;
+            item.cost = (item.count * parseInt(item.price)).toFixed(2);
         }
     });
     return itemList;
@@ -65,28 +70,27 @@ const calculateItemCost = (itemList) => {
 
 const getTotalPrice = (priceList) => {
     return priceList.reduce((acc, cur) => {
-        return acc + cur
+        return parseFloat(acc) + parseFloat(cur)
     }, 0);
 };
 
 
 const calculatePrice = (itemList) => {
-    let result = getItemPromotion(itemList);
     let sum = getTotalPrice(itemList.map(item => {
-        return item.price
+        return item.cost;
     }));
-    return {result, sum};
+    return {itemList, sum};
 };
 
 const getReceipt = (result) => {
     let resultReceipt = '***<没钱赚商店>收据***\n';
-    let itemList = result.result;
+    let itemList = result.itemList;
     let sum = 0;
     itemList.forEach(item => {
-        resultReceipt += `名称：${item.name}，数量：${item.count},${item.unit}，单价：${item.price}(元)，小计：${item.cost}\n`;
+        resultReceipt += `名称：${item.name}，数量：${item.count}${item.unit}，单价：${item.price}(元)，小计：${item.cost}(元)\n`;
         sum += item.price * item.count;
     });
-    resultReceipt += `----------------------\n总计:${result.sum}(元)\n节省:${sum - result.sum}(元)\n`;
+    resultReceipt += `----------------------\n总计：${(result.sum).toFixed(2)}(元)\n节省：${(sum - result.sum).toFixed(2)}(元)\n`;
     resultReceipt += '**********************';
     return resultReceipt;
 };
@@ -97,8 +101,8 @@ const printReceipt = (cartItems) => {
         if (isCartItemValid(item)) return 'have wrong barcode!';
     })
     let itemList = getItemList(cartItems);
-    let result = calculateItemCost(itemList);
-    let receipt = getReceipt(result);
+    let resultList = calculateItemCost(itemList);
+    let receipt = getReceipt(calculatePrice(resultList));
 
     return receipt;
 };
--- test/main-test.js
+++ test/main-test.js
@@ -2,34 +2,31 @@
 
 describe('pos', () => {
 
-//   it('should print text', () => {
-//
-//     const tags = [
-//       'ITEM000001',
-//       'ITEM000001',
-//       'ITEM000001',
-//       'ITEM000001',
-//       'ITEM000001',
-//       'ITEM000003-2.5',
-//       'ITEM000005',
-//       'ITEM000005-2',
-//     ];
-//
-//     spyOn(console, 'log');
-//
-//     printReceipt(tags);
-//
-//     const expectText = `***<没钱赚商店>收据***
-// 名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
-// 名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
-// 名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
-// ----------------------
-// 总计：58.50(元)
-// 节省：7.50(元)
-// **********************`;
-//
-//     expect(console.log).toHaveBeenCalledWith(expectText);
-//   });
+  it('should print text', () => {
+
+    const tags = [
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000003-2.5',
+      'ITEM000005',
+      'ITEM000005-2',
+    ];
+
+    const expectText = `***<没钱赚商店>收据***
+名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
+名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
+名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
+----------------------
+总计：58.50(元)
+节省：7.50(元)
+**********************`;
+
+    expect(printReceipt(tags)).toEqual(expectText);
+  });
+
 
   it('should return true when call isCartItemValid', () => {
     const tags = [
@@ -44,13 +41,56 @@ describe('pos', () => {
     ];
 
     const result = [
-        {barcode:'ITEM000001',
-        name: '雪碧',
-        unit: '瓶',
-        price: '3.00',
-        count: 0,
-        promotion: '',
-        cost: 0.0}];
-    expect(isCartItemValid(tags)).toBe(result);
+        {barcode: "ITEM000001",
+            cost: 0,
+            count: 1,
+            name: "雪碧",
+            price: "3.00",
+            promotion: "",
+            unit: "瓶"}];
+    expect(getItemList(tags)).toEqual(result);
   });
+
+    it('should return itemPromotion when call getItemPromotion', () => {
+        const tags = [
+            {barcode: "ITEM000001",
+                cost: 0,
+                count: 1,
+                name: "雪碧",
+                price: "3.00",
+                promotion: "BUY_TWO_GET_ONE_FREE",
+                unit: "瓶"}];
+
+        const result = [
+            {barcode: "ITEM000001",
+                cost: 0,
+                count: 1,
+                name: "雪碧",
+                price: "3.00",
+                promotion: "BUY_TWO_GET_ONE_FREE",
+                unit: "瓶"}];
+        expect(getItemPromotion(tags)).toEqual(result);
+    });
+
+    it('should return ItemList when call calculateItemCost', () => {
+        const tags = [
+            {barcode: "ITEM000001",
+                cost: 0,
+                count: 2,
+                name: "雪碧",
+                price: "3.00",
+                promotion: "BUY_TWO_GET_ONE_FREE",
+                unit: "瓶"}];
+
+        const result = [
+            {barcode: "ITEM000001",
+                cost: "6.00",
+                count: 2,
+                name: "雪碧",
+                price: "3.00",
+                promotion: "BUY_TWO_GET_ONE_FREE",
+                unit: "瓶"}];
+        expect(calculateItemCost(tags)).toEqual(result);
+    });
+
 });
